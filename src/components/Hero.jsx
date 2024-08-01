2b91a7ecda6b827855d260957898ecd160159e50
tabbitha garcia <tabby.garcia1@gmail.com>
2024-08-01 14:39:08 -0600

    added social links and image in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,25 @@ const Hero = () => {
     <div className='bg-emerald-100 py-24'>
       <div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8'>
         <article>
-          <h1 className='text-7xl fornt-bold tracking-wider'>I'm Tabby</h1>
+          <h1 className='text-7xl fornt-bold tracking-wider'> Hi, I'm Tabby</h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
             Full Stack Developer
           </p>
+          <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+            turning ideas into interactive reality
+          </p>
+          <div className='flex gap-x-4 mt-4'>
+            <a href='https://github.com/tabby-lab'>
+              <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+            <a href='https://www.linkedin.com/in/tabby-garcia-01164a144/'>
+              <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+          </div>
+        </article>
+
+        <article className='hidden md:block '>
+          <img src={heroImg} className='h-80 lg:h-96' />
         </article>
       </div>
     </div>
