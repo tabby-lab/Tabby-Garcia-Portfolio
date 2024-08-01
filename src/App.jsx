cd4a6ff5b00244adfd4d27ad1bdf685e794798a8
tabbitha garcia <tabbitha@virtualintelligencebriefing.com>
2024-08-01 12:24:39 -0600

    building out the nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
-    <div>
-      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
