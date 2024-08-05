3b8a2ad16a7037954aa365a6582167a41e211f5a
tabbitha garcia <tabby.garcia1@gmail.com>
2024-08-05 17:02:23 -0600

    delete projects for now

--- src/data.jsx
+++ src/data.jsx
@@ -5,7 +5,7 @@ export const links = [
   { id: nanoid(), href: '#home', text: 'home' },
   { id: nanoid(), href: '#skills', text: 'skills' },
   { id: nanoid(), href: '#about', text: 'about' },
-  { id: nanoid(), href: '#projects', text: 'projects' },
+  // { id: nanoid(), href: '#projects', text: 'projects' },
 ]
 
 export const skills = [
